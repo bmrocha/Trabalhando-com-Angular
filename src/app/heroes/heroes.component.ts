6526de2211425dd589b1ebe8132ede9f6feea45f
bmrocha7l <bmrocha7l@gmail.com>
2021-12-28 00:02:56 -0300

    ajuste hero let

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { hero } from '../hero.model';
+import { HEROES } from '../mock-heros';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
-  hero: hero = {
+export class HeroesComponent {
+ hero: hero = {
     id: 1,
     name: 'Homem Aranha',
-
   };
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  heroes = HEROES;
 }
---
+++ src/app/mock-heros.ts
@@ -0,0 +1,6 @@
+import { hero } from "./hero.model";
+export const HEROES: hero[] = [
+  {id: 3, name: 'hulk' },
+  {id: 4, name: 'ARANHA' },
+  {id: 5, name: 'HOME DE FERRO' },
+];
